4c55baab72a3361d23bdb859ddc6c77c1282720a
Ahmed Dusmatov <dusmatov9999@gmail.com>
2019-08-21 14:49:39 +0500

    fix(product): Fixed quantity input for product
    Increment and Decrement

--- woocommerce/checkout/review-order.php
+++ woocommerce/checkout/review-order.php
@@ -94,7 +94,7 @@ defined('ABSPATH') || exit;
 
             <?php foreach (WC()->cart->get_coupons() as $code => $coupon) : ?>
                 <tr class="cart-discount coupon-<?php echo esc_attr(sanitize_title($code)); ?>">
-                    <th><?php wc_cart_totals_coupon_label($coupon); ?></th>
+                    <td><?php wc_cart_totals_coupon_label($coupon); ?></td>
                     <td><?php wc_cart_totals_coupon_html($coupon); ?></td>
                 </tr>
             <?php endforeach; ?>
@@ -111,7 +111,7 @@ defined('ABSPATH') || exit;
 
             <?php foreach (WC()->cart->get_fees() as $fee) : ?>
                 <tr class="fee">
-                    <th><?php echo esc_html($fee->name); ?></th>
+                    <td><?php echo esc_html($fee->name); ?></td>
                     <td><?php wc_cart_totals_fee_html($fee); ?></td>
                 </tr>
             <?php endforeach; ?>
@@ -120,13 +120,13 @@ defined('ABSPATH') || exit;
                 <?php if ('itemized' === get_option('woocommerce_tax_total_display')) : ?>
                     <?php foreach (WC()->cart->get_tax_totals() as $code => $tax) : // phpcs:ignore WordPress.WP.GlobalVariablesOverride.OverrideProhibited ?>
                         <tr class="tax-rate tax-rate-<?php echo esc_attr(sanitize_title($code)); ?>">
-                            <th><?php echo esc_html($tax->label); ?></th>
+                            <td><?php echo esc_html($tax->label); ?></td>
                             <td><?php echo wp_kses_post($tax->formatted_amount); ?></td>
                         </tr>
                     <?php endforeach; ?>
                 <?php else : ?>
                     <tr class="tax-total">
-                        <th><?php echo esc_html(WC()->countries->tax_or_vat()); ?></th>
+                        <td><?php echo esc_html(WC()->countries->tax_or_vat()); ?></td>
                         <td><?php wc_cart_totals_taxes_total_html(); ?></td>
                     </tr>
                 <?php endif; ?>
--- woocommerce/global/quantity-input.php
+++ woocommerce/global/quantity-input.php
@@ -31,19 +31,18 @@ if ( $max_value && $min_value === $max_value ) {
     <div class="cost-amount">
 	<div class="quantity custom-input-number size-2">
 		<?php do_action( 'woocommerce_before_quantity_input_field' ); ?>
-		<!--<label class="screen-reader-text" for="<?php echo esc_attr( $input_id ); ?>"><?php echo esc_attr( $label ); ?></label>-->
+		<!--<label class="screen-reader-text" for="<?php //echo esc_attr( $input_id ); ?>"><?php //echo esc_attr( $label ); ?></label>-->
         <button type="button" class="decrement"><span></span></button>
 		<input
 			type="number"
 			id="<?php echo esc_attr( $input_id ); ?>"
-			class="<?php echo esc_attr( join( ' ', (array) $classes ) ); ?> input-field"
+			class="input-field <?php echo esc_attr( join( ' ', (array) $classes ) ); ?> "
 			step="<?php echo esc_attr( $step ); ?>"
 			min="<?php echo esc_attr( $min_value ); ?>"
 			max="<?php echo esc_attr( 0 < $max_value ? $max_value : '' ); ?>"
 			name="<?php echo esc_attr( $input_name ); ?>"
 			value="<?php echo esc_attr( $input_value ); ?>"
 			title="<?php echo esc_attr_x( 'Qty', 'Product quantity input tooltip', 'woocommerce' ); ?>"
-			size="4"
             readonly=""
 			inputmode="<?php echo esc_attr( $inputmode ); ?>" />
         <button type="button" class="increment"><span></span></button>
